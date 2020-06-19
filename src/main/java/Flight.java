8f25001710742f359fe657929228adcf48302c82
cjscrimmy <craigscrimgeour@yahoo.com>
2020-06-19 15:08:47 +0100

    set up flight class and flight testclass. test getting the plane attached to a flight. test passes

---
+++ src/main/java/Flight.java
@@ -0,0 +1,11 @@
+public class Flight {
+    private Plane plane;
+
+    public Flight(Plane plane){
+        this.plane = plane;
+    }
+
+    public Plane getPlane(){
+        return this.plane;
+    }
+}
---
+++ src/test/java/FlightTest.java
@@ -0,0 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FlightTest {
+    private Plane plane;
+    private Passenger passenger1;
+    private Passenger passenger2;
+    private Passenger passenger3;
+    private Passenger passenger4;
+    private Passenger passenger5;
+    private Flight flight;
+
+    @Before
+    public void setUp(){
+        plane = new Plane(PlaneType.CESNER);
+        passenger1 = new Passenger("Juan", 1);
+        passenger2 = new Passenger("Liz", 1);
+        passenger3 = new Passenger("Brian", 2);
+        passenger4 = new Passenger("Sarah", 2);
+        passenger5 = new Passenger("Sergio", 2);
+        flight = new Flight(plane);
+    }
+
+    @Test
+    public void canGetPlane(){
+        assertEquals(plane, flight.getPlane());
+    }
+}
